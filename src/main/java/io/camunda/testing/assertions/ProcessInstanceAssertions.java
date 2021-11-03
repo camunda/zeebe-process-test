a24255f26974b42f604721a9f68982d8571d9e80
Remco Westerhoud <remco@westerhoud.nl>
2021-11-03 11:35:12 +0100

    assertions(pi): Add javadoc to assertions

--- src/main/java/io/camunda/testing/assertions/ProcessInstanceAssertions.java
+++ src/main/java/io/camunda/testing/assertions/ProcessInstanceAssertions.java
@@ -38,6 +38,12 @@ public class ProcessInstanceAssertions
     return new ProcessInstanceAssertions(actual);
   }
 
+  /**
+   * Verifies the expectation that the process instance is started. This will also be true when the
+   * process has been completed or terminated.
+   *
+   * @return this {@link ProcessInstanceAssertions}
+   */
   public ProcessInstanceAssertions isStarted() {
     final Optional<Record<ProcessInstanceRecordValue>> processInstanceRecord =
         StreamFilter.processInstance(recordStreamSource.processInstanceRecords())
@@ -52,6 +58,11 @@ public class ProcessInstanceAssertions
     return this;
   }
 
+  /**
+   * Verifies the expectation that the process instance is completed.
+   *
+   * @return this {@link ProcessInstanceAssertions}
+   */
   public ProcessInstanceAssertions isCompleted() {
     final Optional<Record<ProcessInstanceRecordValue>> processInstanceRecord =
         StreamFilter.processInstance(recordStreamSource.processInstanceRecords())
@@ -67,6 +78,11 @@ public class ProcessInstanceAssertions
     return this;
   }
 
+  /**
+   * Verifies the expectation that the process instance is terminated.
+   *
+   * @return this {@link ProcessInstanceAssertions}
+   */
   public ProcessInstanceAssertions isTerminated() {
     final Optional<Record<ProcessInstanceRecordValue>> processInstanceRecord =
         StreamFilter.processInstance(recordStreamSource.processInstanceRecords())
@@ -82,14 +98,36 @@ public class ProcessInstanceAssertions
     return this;
   }
 
+  /**
+   * Verifies the expectation that the process instance has passed an element with a specific
+   * element id exactly one time.
+   *
+   * @param elementId The id of the element
+   * @return this {@link ProcessInstanceAssertions}
+   */
   public ProcessInstanceAssertions hasPassed(final String elementId) {
     return hasPassed(elementId, 1);
   }
 
+  /**
+   * Verifies the expectation that the process instance has not passed an element with a specific
+   * element id.
+   *
+   * @param elementId The id of the element
+   * @return this {@link ProcessInstanceAssertions}
+   */
   public ProcessInstanceAssertions hasNotPassed(final String elementId) {
     return hasPassed(elementId, 0);
   }
 
+  /**
+   * Verifies the expectation that the process instance has passed an element with a specific
+   * element id exactly N amount of times.
+   *
+   * @param elementId The id of the element
+   * @param times The amount of times the element should be passed
+   * @return this {@link ProcessInstanceAssertions}
+   */
   public ProcessInstanceAssertions hasPassed(final String elementId, final int times) {
     final long count =
         StreamFilter.processInstance(recordStreamSource.processInstanceRecords())
@@ -105,6 +143,13 @@ public class ProcessInstanceAssertions
     return this;
   }
 
+  /**
+   * Verifies the expectation that the process instance is currently waiting at one or more
+   * specified elements.
+   *
+   * @param elementIds The ids of the elements
+   * @return this {@link ProcessInstanceAssertions}
+   */
   public ProcessInstanceAssertions isWaitingAt(final String... elementIds) {
     final List<String> notActivatedElements =
         Arrays.stream(elementIds)
@@ -121,6 +166,13 @@ public class ProcessInstanceAssertions
     return this;
   }
 
+  /**
+   * Verifies the expectation that the process instance is currently not waiting at one or more
+   * specified elements.
+   *
+   * @param elementIds The ids of the elements
+   * @return this {@link ProcessInstanceAssertions}
+   */
   public ProcessInstanceAssertions isNotWaitingAt(final String... elementIds) {
     final List<String> activatedElements =
         Arrays.stream(elementIds).filter(this::isWaitingAtElement).collect(Collectors.toList());
@@ -137,6 +189,13 @@ public class ProcessInstanceAssertions
 
   // TODO if waiting at any element with this id returns true. Maybe add a counter so we can check
   // we are waiting at multiple elements with the same id
+
+  /**
+   * Checks if the process instance is currently waiting at an element with a specific element id.
+   *
+   * @param elementId The id of the element
+   * @return boolean indicating whether the process instance is waiting at the element
+   */
   private boolean isWaitingAtElement(final String elementId) {
     final Map<Long, List<Record<ProcessInstanceRecordValue>>> recordsMap =
         StreamFilter.processInstance(recordStreamSource.processInstanceRecords())
@@ -159,6 +218,13 @@ public class ProcessInstanceAssertions
     return false;
   }
 
+  /**
+   * Verifies the expectation that the process instance is currently waiting to receive one or more
+   * specified messages.
+   *
+   * @param messageNames Names of the messages
+   * @return this {@link ProcessInstanceAssertions}
+   */
   public ProcessInstanceAssertions isWaitingForMessage(final String... messageNames) {
     final List<String> notCreatedProcessMessageSubscriptions =
         Arrays.stream(messageNames)
@@ -177,6 +243,13 @@ public class ProcessInstanceAssertions
     return this;
   }
 
+  /**
+   * Verifies the expectation that the process instance is currently not waiting to receive one or
+   * more specified messages.
+   *
+   * @param messageNames Names of the messages
+   * @return this {@link ProcessInstanceAssertions}
+   */
   public ProcessInstanceAssertions isNotWaitingForMessage(final String... messageNames) {
     final List<String> createdProcessMessageSubscriptions =
         Arrays.stream(messageNames).filter(this::isWaitingForMessage).collect(Collectors.toList());
@@ -193,6 +266,12 @@ public class ProcessInstanceAssertions
     return this;
   }
 
+  /**
+   * Checks if the process instance is currently waiting for a message with a specific message name.
+   *
+   * @param messageName The name of the message
+   * @return boolean indicating whether the process instance is waiting for the message
+   */
   private boolean isWaitingForMessage(final String messageName) {
     final Map<Long, List<Record<ProcessMessageSubscriptionRecordValue>>> recordsMap =
         StreamFilter.processMessageSubscription(
