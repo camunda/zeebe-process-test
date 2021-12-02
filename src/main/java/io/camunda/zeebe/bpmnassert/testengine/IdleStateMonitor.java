c11ab6819830e36c68a9796a735368c6ec0b6970
pihme <pihme@users.noreply.github.com>
2021-12-02 13:03:17 +0100

    refactor: add JavaDoc and renaming

--- src/main/java/io/camunda/zeebe/bpmnassert/testengine/IdleStateMonitor.java
+++ src/main/java/io/camunda/zeebe/bpmnassert/testengine/IdleStateMonitor.java
@@ -10,16 +10,33 @@ import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
 
+/**
+ * Monitor that waits for an idle state. Idle state is a state in which the process engine makes no
+ * progress and is waiting for new commands or events to trigger</br>
+ *
+ * <p>On a technical level, idle state is defined by
+ *
+ * <ul>
+ *   <li>The point in time when all current records in the commit log have been processed by the
+ *       engine
+ *   <li>This is insufficient, however, because the engine might still be in the process of writing
+ *       follow-up records
+ *   <li>Therefore, when the first idle state is detected, a grace period starts. If no new records
+ *       come in during that grace period, then at the end onf the grace period callbacks are
+ *       notified
+ * </ul>
+ */
 final class IdleStateMonitor implements LogStorage.CommitListener, StreamProcessorListener {
 
   private static final Timer TIMER = new Timer();
+  public static final int GRACE_PERIOD = 10;
   private final List<Runnable> callbacks = new ArrayList<>();
   private final LogStreamReader reader;
   private volatile long lastEventPosition = -1L;
   private volatile long lastProcessedPosition = -1L;
 
-  private volatile TimerTask task =
-      createTask(); // must never be null for the synchronization to work
+  private volatile TimerTask idleStateNotifier =
+      createIdleStateNotifier(); // must never be null for the synchronization to work
 
   IdleStateMonitor(final InMemoryLogStorage logStorage, final LogStreamReader logStreamReader) {
     logStorage.addCommitListener(this);
@@ -36,19 +53,27 @@ final class IdleStateMonitor implements LogStorage.CommitListener, StreamProcess
 
   private void checkIdleState() {
     if (isInIdleState()) {
-      synchronized (task) {
-        task = createTask();
-        try {
-          TIMER.schedule(task, 10);
-        } catch (IllegalStateException e) {
-          // thrown - among others - if task was cancelled before it could be scheduled
-          // do nothing in this case
-        }
-      }
+      scheduleNotification();
     } else {
-      synchronized (task) {
-        task.cancel();
+      cancelNotification();
+    }
+  }
+
+  private void scheduleNotification() {
+    synchronized (idleStateNotifier) {
+      idleStateNotifier = createIdleStateNotifier();
+      try {
+        TIMER.schedule(idleStateNotifier, GRACE_PERIOD);
+      } catch (IllegalStateException e) {
+        // thrown - among others - if task was cancelled before it could be scheduled
+        // do nothing in this case
       }
+    }
+  }
+
+  private void cancelNotification() {
+    synchronized (idleStateNotifier) {
+      idleStateNotifier.cancel();
     }
   }
 
@@ -88,7 +113,7 @@ final class IdleStateMonitor implements LogStorage.CommitListener, StreamProcess
     checkIdleState();
   }
 
-  private TimerTask createTask() {
+  private TimerTask createIdleStateNotifier() {
     return new TimerTask() {
       @Override
       public void run() {
