4081ea1c55209bbb88e4560442b9542a0971f359
Remco Westerhoud <remco@westerhoud.nl>
2022-02-17 15:03:43 +0100

    feat: start engine upon starting agent

--- engine-agent/src/main/java/io/camunda/zeebe/process/test/engine/agent/ZeebeProcessTestEngine.java
+++ engine-agent/src/main/java/io/camunda/zeebe/process/test/engine/agent/ZeebeProcessTestEngine.java
@@ -11,9 +11,9 @@ public class ZeebeProcessTestEngine {
   public static void main(String[] args) throws IOException {
     final InMemoryEngine engine = EngineFactory.create();
     final EngineControlImpl engineService = new EngineControlImpl(engine);
-
     final Server server = ServerBuilder.forPort(26501).addService(engineService).build();
 
+    engine.start();
     server.start();
   }
 }
