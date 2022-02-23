18820ce9534a7212158d9f9a4ca8df3f26df9896
Remco Westerhoud <remco@westerhoud.nl>
2022-02-23 16:46:11 +0100

    docs: add javadoc to ZeebeProcessTest annotations

--- extension-testcontainer/src/main/java/io.camunda.zeebe.process.test.extension.testcontainer/ZeebeProcessTest.java
+++ extension-testcontainer/src/main/java/io.camunda.zeebe.process.test.extension.testcontainer/ZeebeProcessTest.java
@@ -7,8 +7,36 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 import org.junit.jupiter.api.extension.ExtendWith;
 
+/**
+ * This annotation can be used to test BPMN processes. It will make use of testcontainers to run an
+ * in memory engine. To use this annotation Java 8 or higher is required. Docker also needs to be
+ * running.
+ *
+ * Annotating test classes with this annotation will do a couple of things:
+ *
+ * <ul>
+ *   <li>It start a docker container running and in memory engine.
+ *   <li>It will create a client which can be used to interact with the engine.
+ *   <li>It will (optionally) inject 3 fields in your test class:
+ *       <ul>
+ *         <li>InMemoryEngine - This is the engine that will run your process. It will provide some
+ *             basic functionality to help you write your tests, such as waiting for an idle state
+ *             and increasing the time.
+ *         <li>ZeebeClient - This is the client that allows you to communicate with the engine. It
+ *             allows you to send commands to the engine.
+ *         <li>RecordStream - This gives you access to all the records that are processed by
+ *             the engine. It is what the assertions use to verify expectations. This grants you the
+ *             freedom to create your own assertions.
+ *       </ul>
+ *   <li>It will take care of cleaning up the engine and client when the testcase is finished.
+ * </ul>
+ *
+ * @since Java 8
+ */
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 @Inherited
 @ExtendWith(ZeebeProcessTestExtension.class)
-public @interface ZeebeProcessTest {}
+public @interface ZeebeProcessTest {
+
+}
--- extension/src/main/java/io/camunda/zeebe/process/test/extension/ZeebeProcessTest.java
+++ extension/src/main/java/io/camunda/zeebe/process/test/extension/ZeebeProcessTest.java
@@ -7,6 +7,31 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 import org.junit.jupiter.api.extension.ExtendWith;
 
+/**
+ * This annotation can be used to test BPMN processes. It will run an in memory Zeebe engine. To use
+ * this annotation Java 17 or higher is required.
+ *
+ * Annotating test classes with this annotation will do a couple of things:
+ *
+ * <ul>
+ *   <li>It will create and start an in memory engine. This will be a new engine for each testcase.
+ *   <li>It will create a client which can be used to interact with the engine.
+ *   <li>It will (optionally) inject 3 fields in your test class:
+ *       <ul>
+ *         <li>InMemoryEngine - This is the engine that will run your process. It will provide some
+ *             basic functionality to help you write your tests, such as waiting for an idle state
+ *             and increasing the time.
+ *         <li>ZeebeClient - This is the client that allows you to communicate with the engine. It
+ *             allows you to send commands to the engine.
+ *         <li>RecordStream - This gives you access to all the records that are processed by
+ *             the engine. It is what the assertions use to verify expectations. This grants you the
+ *             freedom to create your own assertions.
+ *       </ul>
+ *   <li>It will take care of cleaning up the engine and client when the testcase is finished.
+ * </ul>
+ *
+ * @since Java 17
+ */
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 @Inherited
