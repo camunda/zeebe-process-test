628713736ee60de0cf72978004d5e454bee70a85
Remco Westerhoud <remco@westerhoud.nl>
2022-03-11 14:27:29 +0100

    fix: automatic license issues
    Automatically adding the copyright headers included a new line. The maven license plugin does not want this new line, so I have removed this from the configuration.

--- qa/src/test/java/io/camunda/zeebe/process/test/qa/ExtendAbstractsTest.java
+++ qa/src/test/java/io/camunda/zeebe/process/test/qa/ExtendAbstractsTest.java
@@ -12,9 +12,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
  */
-
 package io.camunda.zeebe.process.test.qa;
 
 import com.google.common.reflect.ClassPath;
