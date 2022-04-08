3b76c460cd62523ba1ff7e8a511fec7365e6d25a
Remco Westerhoud <remco@westerhoud.nl>
2022-04-08 17:18:04 +0200

    test: all gateway endpoints are implemented
    This test checks if all the gateway endpoints are implemented. Since gRPC does not provide an interface which can force this during compile time we are at risk that updating the Zeebe version could result in ZPT not supporting all endpoints that can be triggered from the client. To circumvent this problem this test was created to make sure we implement all the methods gRPC expects us to.

---
+++ engine/src/test/java/io/camunda/zeebe/process/test/engine/GrpcToLogStreamGatewayTest.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Licensed under the Zeebe Community License 1.1. You may not use this file
+ * except in compliance with the Zeebe Community License 1.1.
+ */
+
+package io.camunda.zeebe.process.test.engine;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import io.camunda.zeebe.gateway.protocol.GatewayGrpc.GatewayImplBase;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.Optional;
+import java.util.stream.Stream;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+class GrpcToLogStreamGatewayTest {
+
+  @ParameterizedTest(name = "{0}")
+  @MethodSource("provideMethods")
+  void testImplementsGatewayEndpoint(final String methodName) {
+    final Optional<Method> optionalMethod =
+        Arrays.stream(GrpcToLogStreamGateway.class.getDeclaredMethods())
+            .filter(m -> m.getName().equals(methodName))
+            .findAny();
+
+    assertThat(optionalMethod)
+        .describedAs("Expected method %s to be implemented", methodName)
+        .isPresent();
+  }
+
+  private static Stream<Arguments> provideMethods() {
+    return Arrays.stream(GatewayImplBase.class.getDeclaredMethods())
+        .filter(method -> !method.getName().equals("bindService"))
+        .map(method -> Arguments.of(method.getName()));
+  }
+}
