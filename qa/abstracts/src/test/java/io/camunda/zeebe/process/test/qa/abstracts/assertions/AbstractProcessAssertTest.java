1ee34fc3287c5307abb2d4d456afd65e44b1b15c
Remco Westerhoud <remco@westerhoud.nl>
2022-04-11 10:09:48 +0200

    refactor: split QA in separate modules
    Only files have been moved. No code has been changed.

---
+++ qa/abstracts/src/test/java/io/camunda/zeebe/process/test/qa/abstracts/assertions/AbstractProcessAssertTest.java
@@ -0,0 +1,257 @@
+/*
+ * Copyright © 2021 camunda services GmbH (info@camunda.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.camunda.zeebe.process.test.qa.abstracts.assertions;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import io.camunda.zeebe.client.ZeebeClient;
+import io.camunda.zeebe.client.api.response.DeploymentEvent;
+import io.camunda.zeebe.process.test.api.ZeebeTestEngine;
+import io.camunda.zeebe.process.test.assertions.BpmnAssert;
+import io.camunda.zeebe.process.test.assertions.ProcessAssert;
+import io.camunda.zeebe.process.test.qa.abstracts.util.Utilities;
+import io.camunda.zeebe.process.test.qa.abstracts.util.Utilities.ProcessPackLoopingServiceTask;
+import java.util.concurrent.TimeoutException;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+public abstract class AbstractProcessAssertTest {
+
+  public static final String WRONG_VALUE = "wrong value";
+
+  // These tests are for testing assertions as well as examples for users
+  @Nested
+  class HappyPathTests {
+
+    private ZeebeClient client;
+    private ZeebeTestEngine engine;
+
+    @Test
+    void testHasBPMNProcessId() throws InterruptedException, TimeoutException {
+      // given
+      final DeploymentEvent deploymentEvent =
+          Utilities.deployProcess(client, ProcessPackLoopingServiceTask.RESOURCE_NAME);
+
+      // when
+      final ProcessAssert processAssert =
+          BpmnAssert.assertThat(deploymentEvent)
+              .extractingProcessByBpmnProcessId(ProcessPackLoopingServiceTask.PROCESS_ID);
+
+      // then
+      processAssert.hasBpmnProcessId(ProcessPackLoopingServiceTask.PROCESS_ID);
+    }
+
+    @Test
+    void testHasVersion() throws InterruptedException, TimeoutException {
+      // given
+      final DeploymentEvent deploymentEvent =
+          Utilities.deployProcess(client, ProcessPackLoopingServiceTask.RESOURCE_NAME);
+
+      // when
+      final ProcessAssert processAssert =
+          BpmnAssert.assertThat(deploymentEvent)
+              .extractingProcessByBpmnProcessId(ProcessPackLoopingServiceTask.PROCESS_ID);
+
+      // then
+      processAssert.hasVersion(1);
+    }
+
+    @Test
+    void testHasResourceName() throws InterruptedException, TimeoutException {
+      // given
+      final DeploymentEvent deploymentEvent =
+          Utilities.deployProcess(client, ProcessPackLoopingServiceTask.RESOURCE_NAME);
+
+      // when
+      final ProcessAssert processAssert =
+          BpmnAssert.assertThat(deploymentEvent)
+              .extractingProcessByBpmnProcessId(ProcessPackLoopingServiceTask.PROCESS_ID);
+
+      // then
+      processAssert.hasResourceName(ProcessPackLoopingServiceTask.RESOURCE_NAME);
+    }
+
+    @Test
+    void testHasAnyInstances() throws InterruptedException, TimeoutException {
+      // given
+      final DeploymentEvent deploymentEvent =
+          Utilities.deployProcess(client, ProcessPackLoopingServiceTask.RESOURCE_NAME);
+
+      // when
+      Utilities.startProcessInstance(engine, client, ProcessPackLoopingServiceTask.PROCESS_ID);
+
+      final ProcessAssert processAssert =
+          BpmnAssert.assertThat(deploymentEvent)
+              .extractingProcessByBpmnProcessId(ProcessPackLoopingServiceTask.PROCESS_ID);
+
+      // then
+      processAssert.hasAnyInstances();
+    }
+
+    @Test
+    void testHasNoInstances() throws InterruptedException, TimeoutException {
+      // given
+      final DeploymentEvent deploymentEvent =
+          Utilities.deployProcess(client, ProcessPackLoopingServiceTask.RESOURCE_NAME);
+
+      // when
+      final ProcessAssert processAssert =
+          BpmnAssert.assertThat(deploymentEvent)
+              .extractingProcessByBpmnProcessId(ProcessPackLoopingServiceTask.PROCESS_ID);
+
+      // then
+      processAssert.hasNoInstances();
+    }
+
+    @Test
+    void testHasInstances() throws InterruptedException, TimeoutException {
+      // given
+      final DeploymentEvent deploymentEvent =
+          Utilities.deployProcess(client, ProcessPackLoopingServiceTask.RESOURCE_NAME);
+
+      // when
+      Utilities.startProcessInstance(engine, client, ProcessPackLoopingServiceTask.PROCESS_ID);
+      Utilities.startProcessInstance(engine, client, ProcessPackLoopingServiceTask.PROCESS_ID);
+
+      final ProcessAssert processAssert =
+          BpmnAssert.assertThat(deploymentEvent)
+              .extractingProcessByBpmnProcessId(ProcessPackLoopingServiceTask.PROCESS_ID);
+
+      // then
+      processAssert.hasInstances(2);
+    }
+  }
+
+  // These tests are just for assertion testing purposes. These should not be used as examples.
+  @Nested
+  class UnhappyPathTests {
+
+    private ZeebeClient client;
+    private ZeebeTestEngine engine;
+
+    @Test
+    void testHasBPMNProcessIdFailure() throws InterruptedException, TimeoutException {
+      // given
+      final DeploymentEvent deploymentEvent =
+          Utilities.deployProcess(client, ProcessPackLoopingServiceTask.RESOURCE_NAME);
+
+      // when
+      final ProcessAssert processAssert =
+          BpmnAssert.assertThat(deploymentEvent)
+              .extractingProcessByBpmnProcessId(ProcessPackLoopingServiceTask.PROCESS_ID);
+
+      // then
+      assertThatThrownBy(() -> processAssert.hasBpmnProcessId(WRONG_VALUE))
+          .isInstanceOf(AssertionError.class)
+          .hasMessage(
+              "Expected BPMN process ID to be '%s' but was '%s' instead.",
+              WRONG_VALUE, ProcessPackLoopingServiceTask.PROCESS_ID);
+    }
+
+    @Test
+    void testHasVersionFailure() throws InterruptedException, TimeoutException {
+      // given
+      final DeploymentEvent deploymentEvent =
+          Utilities.deployProcess(client, ProcessPackLoopingServiceTask.RESOURCE_NAME);
+
+      // when
+      final ProcessAssert processAssert =
+          BpmnAssert.assertThat(deploymentEvent)
+              .extractingProcessByBpmnProcessId(ProcessPackLoopingServiceTask.PROCESS_ID);
+
+      // then
+      assertThatThrownBy(() -> processAssert.hasVersion(12345))
+          .isInstanceOf(AssertionError.class)
+          .hasMessage("Expected version to be 12345 but was 1 instead");
+    }
+
+    @Test
+    void testHasResourceNameFailure() throws InterruptedException, TimeoutException {
+      // given
+      final DeploymentEvent deploymentEvent =
+          Utilities.deployProcess(client, ProcessPackLoopingServiceTask.RESOURCE_NAME);
+
+      // when
+      final ProcessAssert processAssert =
+          BpmnAssert.assertThat(deploymentEvent)
+              .extractingProcessByBpmnProcessId(ProcessPackLoopingServiceTask.PROCESS_ID);
+
+      // then
+      assertThatThrownBy(() -> processAssert.hasResourceName(WRONG_VALUE))
+          .isInstanceOf(AssertionError.class)
+          .hasMessage(
+              "Expected resource name to be '%s' but was '%s' instead.",
+              WRONG_VALUE, ProcessPackLoopingServiceTask.RESOURCE_NAME);
+    }
+
+    @Test
+    void testHasAnyInstancesFailure() throws InterruptedException, TimeoutException {
+      // given
+      final DeploymentEvent deploymentEvent =
+          Utilities.deployProcess(client, ProcessPackLoopingServiceTask.RESOURCE_NAME);
+
+      // when
+      final ProcessAssert processAssert =
+          BpmnAssert.assertThat(deploymentEvent)
+              .extractingProcessByBpmnProcessId(ProcessPackLoopingServiceTask.PROCESS_ID);
+
+      // then
+      assertThatThrownBy(processAssert::hasAnyInstances)
+          .isInstanceOf(AssertionError.class)
+          .hasMessage("The process has no instances");
+    }
+
+    @Test
+    void testHasNoInstances() throws InterruptedException, TimeoutException {
+      // given
+      final DeploymentEvent deploymentEvent =
+          Utilities.deployProcess(client, ProcessPackLoopingServiceTask.RESOURCE_NAME);
+
+      // when
+      Utilities.startProcessInstance(engine, client, ProcessPackLoopingServiceTask.PROCESS_ID);
+
+      final ProcessAssert processAssert =
+          BpmnAssert.assertThat(deploymentEvent)
+              .extractingProcessByBpmnProcessId(ProcessPackLoopingServiceTask.PROCESS_ID);
+
+      // then
+      assertThatThrownBy(processAssert::hasNoInstances)
+          .isInstanceOf(AssertionError.class)
+          .hasMessage("The process does have instances");
+    }
+
+    @Test
+    void testHasInstances() throws InterruptedException, TimeoutException {
+      // given
+      final DeploymentEvent deploymentEvent =
+          Utilities.deployProcess(client, ProcessPackLoopingServiceTask.RESOURCE_NAME);
+
+      // when
+      Utilities.startProcessInstance(engine, client, ProcessPackLoopingServiceTask.PROCESS_ID);
+      Utilities.startProcessInstance(engine, client, ProcessPackLoopingServiceTask.PROCESS_ID);
+      Utilities.startProcessInstance(engine, client, ProcessPackLoopingServiceTask.PROCESS_ID);
+
+      final ProcessAssert processAssert =
+          BpmnAssert.assertThat(deploymentEvent)
+              .extractingProcessByBpmnProcessId(ProcessPackLoopingServiceTask.PROCESS_ID);
+
+      // then
+      assertThatThrownBy(() -> processAssert.hasInstances(2))
+          .isInstanceOf(AssertionError.class)
+          .hasMessage("Expected number of instances to be 2 but was 3 instead");
+    }
+  }
+}
---
+++ qa/embedded/src/test/java/io/camunda/zeebe/process/test/qa/embedded/ExtendAbstractsTest.java
@@ -0,0 +1,102 @@
+/*
+ * Copyright © 2021 camunda services GmbH (info@camunda.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.camunda.zeebe.process.test.qa.embedded;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import io.camunda.zeebe.process.test.extension.ZeebeProcessTest;
+import io.github.classgraph.ClassGraph;
+import io.github.classgraph.ClassInfo;
+import io.github.classgraph.ClassInfoList;
+import java.util.stream.Stream;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+class ExtendAbstractsTest {
+
+  public static final Class<ZeebeProcessTest> EMBEDDED_ANNOTATION = ZeebeProcessTest.class;
+  public static final Class<io.camunda.zeebe.process.test.extension.testcontainer.ZeebeProcessTest>
+      TESTCONTAINER_ANNOTATION =
+          io.camunda.zeebe.process.test.extension.testcontainer.ZeebeProcessTest.class;
+  private static final String ABSTRACT_PACKAGE = "io.camunda.zeebe.process.test.qa.abstracts";
+  private static final String EMBEDDED_PACKAGE = "io.camunda.zeebe.process.test.qa.embedded";
+  private static final String TESTCONTAINER_PACKAGE =
+      "io.camunda.zeebe.process.test.qa.testcontainer";
+
+  private static final ClassInfoList EMBEDDED_CLASSES =
+      new ClassGraph()
+          .acceptPackages(EMBEDDED_PACKAGE)
+          .ignoreClassVisibility()
+          .enableAnnotationInfo()
+          .scan()
+          .getAllStandardClasses()
+          .filter(info -> !info.isInnerClass());
+
+  private static final ClassInfoList TESTCONTAINER_CLASSES =
+      new ClassGraph()
+          .acceptPackages(TESTCONTAINER_PACKAGE)
+          .ignoreClassVisibility()
+          .enableAnnotationInfo()
+          .scan()
+          .getAllStandardClasses()
+          .filter(info -> !info.isInnerClass());
+
+  @ParameterizedTest(name = "{0}")
+  @MethodSource("provideAbstractClasses")
+  void testAbstractClassIsExtendedWithEmbeddedExtension(
+      final String className, final Class<?> abstractClass) {
+    final ClassInfoList embeddedClass =
+        EMBEDDED_CLASSES
+            .filter(info -> info.getPackageName().contains("embedded"))
+            .filter(info -> info.extendsSuperclass(abstractClass))
+            .filter(info -> info.hasAnnotation(EMBEDDED_ANNOTATION));
+
+    assertThat(embeddedClass)
+        .withFailMessage(
+            "Expected 1 embedded implementation of %s, but found %d: %s",
+            className, embeddedClass.size(), embeddedClass)
+        .hasSize(1);
+  }
+
+  @ParameterizedTest(name = "{0}")
+  @MethodSource("provideAbstractClasses")
+  void testAbstractClassIsExtendedWithTestcontainerExtension(
+      final String className, final Class<?> abstractClass) {
+    final ClassInfoList testcontainerClass =
+        TESTCONTAINER_CLASSES
+            .filter(info -> info.getPackageName().contains("testcontainer"))
+            .filter(info -> info.extendsSuperclass(abstractClass))
+            .filter(info -> info.hasAnnotation(TESTCONTAINER_ANNOTATION));
+
+    assertThat(testcontainerClass)
+        .withFailMessage(
+            "Expected 1 testcontainer implementation of %s, but found %d: %s",
+            className, testcontainerClass.size(), testcontainerClass)
+        .hasSize(1);
+  }
+
+  private static Stream<Arguments> provideAbstractClasses() {
+    return new ClassGraph()
+            .acceptPackages(ABSTRACT_PACKAGE)
+            .scan()
+            .getAllStandardClasses()
+            .filter(ClassInfo::isAbstract)
+            .filter(info -> !info.isInnerClass())
+            .stream()
+            .map(info -> Arguments.of(info.getSimpleName(), info.loadClass()));
+  }
+}
