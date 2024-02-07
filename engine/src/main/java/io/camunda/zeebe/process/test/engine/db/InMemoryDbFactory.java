4fe785fbc8dd459a05f3942ec7cab4dff2f45b42
Nicolas Pepin-Perreault <nicolas.pepin-perreault@camunda.com>
2024-02-07 20:16:26 +0100

    fix: fix for new EnumValue

--- engine/src/main/java/io/camunda/zeebe/process/test/engine/db/InMemoryDbFactory.java
+++ engine/src/main/java/io/camunda/zeebe/process/test/engine/db/InMemoryDbFactory.java
@@ -9,9 +9,10 @@ package io.camunda.zeebe.process.test.engine.db;
 
 import io.camunda.zeebe.db.ZeebeDb;
 import io.camunda.zeebe.db.ZeebeDbFactory;
+import io.camunda.zeebe.protocol.EnumValue;
 import java.io.File;
 
-public class InMemoryDbFactory<ColumnFamilyTpe extends Enum<ColumnFamilyTpe>>
+public class InMemoryDbFactory<ColumnFamilyTpe extends Enum<ColumnFamilyTpe> & EnumValue>
     implements ZeebeDbFactory<ColumnFamilyTpe> {
 
   public ZeebeDb<ColumnFamilyTpe> createDb() {
