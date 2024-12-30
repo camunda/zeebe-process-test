56fffb730934774e81fa0f5d0ae0bda25c95d37a
Nicolas Pepin-Perreault <nicolas.pepin-perreault@camunda.com>
2024-12-30 14:25:22 +0100

    refactor: migrate API module to camunda-client-java

--- api/src/main/java/io/camunda/zeebe/process/test/api/ZeebeTestEngine.java
+++ api/src/main/java/io/camunda/zeebe/process/test/api/ZeebeTestEngine.java
@@ -16,7 +16,7 @@
 package io.camunda.zeebe.process.test.api;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import io.camunda.zeebe.client.ZeebeClient;
+import io.camunda.client.CamundaClient;
 import java.time.Duration;
 import java.util.concurrent.TimeoutException;
 
@@ -38,14 +38,14 @@ public interface ZeebeTestEngine {
   RecordStreamSource getRecordStreamSource();
 
   /**
-   * @return a newly created {@link ZeebeClient}
+   * @return a newly created {@link CamundaClient}
    */
-  ZeebeClient createClient();
+  CamundaClient createClient();
 
   /**
-   * @return a newly created {@link ZeebeClient} with custom mapper
+   * @return a newly created {@link CamundaClient} with custom mapper
    */
-  ZeebeClient createClient(ObjectMapper customObjectMapper);
+  CamundaClient createClient(ObjectMapper customObjectMapper);
 
   /**
    * @return the address at which the gateway is reachable
