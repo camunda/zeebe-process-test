2360ea0b1bec2f2c8b64ccf1688fef9b5ae81e82
rodrigolourencolopes <rodrigo.lopes@camunda.com>
2025-02-21 18:20:17 +0100

    fix: remove deprecated SpringZeebeTestContext for SpringZeebeTestContext.

--- spring-test/common/src/main/java/io/camunda/zeebe/spring/test/configuration/ZeebeTestDefaultConfiguration.java
+++ spring-test/common/src/main/java/io/camunda/zeebe/spring/test/configuration/ZeebeTestDefaultConfiguration.java
@@ -15,7 +15,7 @@
  */
 package io.camunda.zeebe.spring.test.configuration;
 
-import static io.camunda.zeebe.spring.client.configuration.CamundaAutoConfiguration.DEFAULT_OBJECT_MAPPER;
+import static io.camunda.spring.client.configuration.CamundaAutoConfiguration.DEFAULT_OBJECT_MAPPER;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.camunda.client.api.JsonMapper;
